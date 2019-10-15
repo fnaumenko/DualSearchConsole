2c2ed4e370e74b0ef23897ee3c29523118685620
Fedor Naumenko <30726965+fnaumenko@users.noreply.github.com>
2019-10-15 13:55:35 +0100

    Add files via upload

---
+++ TestSearchConsole/TestSearchConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TestSearchConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string query = string.Empty;
+            while (true)
+            {
+                Console.Write("type query or 'q' to quit: ");
+                if ((query = Console.ReadLine()) == "q") break;
+                //SaveResponse(query);
+                Search(query);
+            }
+        }
+
+        /// <summary>Output searsh results in 2 different pages.</summary>
+        /// <param name="query">search query</param>
+        private static void Search(string query)
+        {
+            if (query == string.Empty) return;
+            System.Diagnostics.Process.Start("http://google.com/search?q=" + query);
+            System.Diagnostics.Process.Start("http://bing.com/search?q=" + query);
+        }
+
+        //private static void SaveResponse(string query)
+        //{
+        //    using (System.Net.WebClient client = new System.Net.WebClient())
+        //    {
+        //        string url = "http://google.com/search?q=" + query;
+        //        string content = client.DownloadString(url);
+        //        System.IO.File.WriteAllText("C:\\Users\\Fedor\\Downloads\\GOOGLE.html", content);
+        //    }
+        //}
+    }
+}
